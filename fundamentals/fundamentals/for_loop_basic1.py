0fa2b6bfae0c5d0504f29e4958459a60cbb6c328
Nikhil Virparia <nvirparia@gmail.com>
2021-08-30 19:26:56 -0400

    For Loop

--- fundamentals/fundamentals/for_loop_basic1.py
+++ fundamentals/fundamentals/for_loop_basic1.py
@@ -1,5 +1,50 @@
 for x in range(0, 151, 1):
     print(x)
-# output: 0, to 150
+# Basic - Print all integers from 0 to 150. 
+
+print('#########')
+
+for x in range(5, 1001, 5):
+    print(x)
+# Multiples of Five - Print all the multiples of 5 from 5 to 1,000. 
+
+print('#########')
+
+for x in range(1, 101, 1):
+    if x % 10 == 0:
+        print("Coding Dojo")
+    elif x % 5 == 0:
+        print("Coding")
+    else:
+        print(x)
+# Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
+
+print('#########')
+
+x = 0
+for add in range(1, 500001, 2):
+    x = x + add
+
+print(x)
+
+print('#########')
+#Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
 
 
+for x in range(2018, 0, -4):
+    print(x)
+
+print('#########')
+
+#Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
+
+lownum = 2
+highnum = 9
+mult = 3
+
+for x in range(lownum, highnum + 1, 1):
+    if x % mult == 0:
+        print(x)
+
+
+#Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult. For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
