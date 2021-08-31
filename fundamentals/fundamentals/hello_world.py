fd6ff47ca430b8429e2cab747064488753fc1484
Nikhil Virparia <nvirparia@gmail.com>
2021-08-30 21:55:19 -0400

    Updated Hello World

--- fundamentals/fundamentals/hello_world.py
+++ fundamentals/fundamentals/hello_world.py
@@ -6,17 +6,17 @@
 #print(y)
 
 # 1. TASK: print "Hello World"
-print("Hello World")
+print( "Hello World" )
 # 2. print "Hello Noelle!" with the name in a variable
 name = "Noelle"
-print("Hello", name)  # with a comma
-print("Hello" + name)  # with a +
+print( "Hello", name,"!" )	# with a comma
+print( "Hello " + name + "!" )	# with a +
 # 3. print "Hello 42!" with the number in a variable
-#name2 = 42
-#print("Hello", name2, "!")  # with a comma
-#print("Hello" + name2 + "!")  # with a +	-- this one should give us an error!
+name = 42
+print("Hello" , 42, "!" )	# with a comma
+print( "Hello " + str(42) + "!")	# with a +	-- this one should give us an error!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
-print("I love to eat sushi and pizza.")  # with .format()
-print("I love to eat sushi and pizza.")  # with an f string
+print( "I love to eat {} and {}.".format(fave_food1,fave_food2) ) # with .format()
+print( f"I love to eat {fave_food1} and {fave_food2}." ) # with an f string
