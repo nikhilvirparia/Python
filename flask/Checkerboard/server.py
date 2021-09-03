ba3cf84f706b65c4727cacdf5723583fd9a74c2d
Nikhil Virparia <nvirparia@gmail.com>
2021-09-02 22:06:48 -0400

    Update Checkerboard

--- flask/Checkerboard/server.py
+++ flask/Checkerboard/server.py
@@ -6,28 +6,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def success():
-    return render_template("index.html", times=0)
-
-# @app.route('/')
-# def index():
-#     # notice the 2 new named arguments!
-#     return render_template("index.html")
-
-#phrase="hello", times=5
-# # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
-# @app.route('/hello/<name>')
-# def hello(name):
-#     print(name)
-#     return "Hello, " + name
+def index():
+    return render_template("index.html", row=8, col=8, color_one='red', color_two='blue')
 
 
-# # for a route '/users/____/____', two parameters in the url get passed as username and id
-# @app.route('/users/<username>/<id>')
-# def show_user_profile(username, id):
-#     print(username)
-#     print(id)
-#     return "username: " + username + ", id: " + id
+@app.route('/<int:x>')
+def row(x):
+    return render_template("index.html", row=x, col=8, color_one='red', color_two='blue')
 
 
 if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
