0731d041397884cbc2993e762a7f9a383cc342ef
Nikhil Virparia <nvirparia@gmail.com>
2021-09-04 18:45:37 -0400

    update routes

--- flask/Checkerboard/server.py
+++ flask/Checkerboard/server.py
@@ -14,6 +14,10 @@ def index():
 def row(x):
     return render_template("index.html", row=x, col=8, color_one='black', color_two='red')
 
+@app.route('/<int:x>/<int:y>')
+def row_col(x,y):
+    return render_template("index.html",row=x,col=y,color_one='black',color_two='red')
+    
 
 if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
     app.run(debug=True)    # Run the app in debug mode.
