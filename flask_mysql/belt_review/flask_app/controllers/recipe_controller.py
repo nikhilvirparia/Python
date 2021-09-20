c1249b50ba62e65a7d0e78d959509b07f40ea0fb
Nikhil Virparia <nvirparia@gmail.com>
2021-09-20 16:21:20 -0400

    Update Recipe

--- flask_mysql/belt_review/flask_app/controllers/recipe_controller.py
+++ flask_mysql/belt_review/flask_app/controllers/recipe_controller.py
@@ -56,12 +56,16 @@ def recipes_instruction(id):
 def add_recipe():
 
     today = date.today()
-
-    return render_template("add.html", today=today)
+    user_id = session['user_id']
+    return render_template("add.html", today=today, user_id=user_id)
 
 
 @app.route("/recipes/create", methods=["POST"])
 def create_recipe():
+
+    # validate Recipe
+    if not Recipe.validate_recipe(request.form):
+        return redirect("/recipes/new")
     
     data = {
         "user_id": session['user_id'],
@@ -76,3 +80,59 @@ def create_recipe():
     Recipe.create_recipe(data)
 
     return redirect("/dashboard")
+
+# ============================
+# Edit Recipe Route
+# ============================
+@app.route("/recipes/edit/<int:recipe_id>/")
+def edit_instruction(recipe_id):
+    today = date.today()
+
+    if not 'user_id' in session:
+        return redirect("/")
+
+    # ID is saved, and return the main
+    data = {
+        "id": recipe_id,
+        "user_id": session['user_id']
+    }
+
+    user = User.get_user_info(data)
+    one_item = Recipe.get_one_recipe(data)
+
+    return render_template("edit.html", user=user, recipes=one_item, today=today)
+
+@app.route('/update/recipe',methods=['POST'])
+def update_recipes():
+    if 'user_id' not in session:
+        return redirect('/logout')
+
+    if not Recipe.validate_recipe(request.form):
+        return redirect("/recipes/edit/")
+        
+    data = {
+        "recipes_id": request.form["recipes_id"],
+        "name": request.form["name"],
+        "description": request.form["description"],
+        "instructions": request.form["instructions"],
+        "date_made": request.form["date_made"],
+        "thirty_mins": request.form["thirty_mins"],
+    }
+    
+    Recipe.update_recipes(data)
+    return redirect('/dashboard')
+    
+# ============================
+# Delete Recipe Route
+# ============================
+
+@app.route('/recipes/delete/<int:id>')
+def delete_recipes(id):
+    data = {
+        'id': id
+    }
+
+    Recipe.delete_recipes(data)
+
+    return redirect('/dashboard')
+
--- flask_mysql/belt_review/flask_app/models/recipe.py
+++ flask_mysql/belt_review/flask_app/models/recipe.py
@@ -1,6 +1,7 @@
 from flask_app import app
 from flask import flash
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models import user
 
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
@@ -16,6 +17,22 @@ class Recipe:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
         self.user_id = data['user_id']
+
+        self.user = {}
+
+    @staticmethod
+    def validate_recipe(form_data):
+        is_valid = True
+        if len(form_data['name']) < 3:
+            flash("Recipe name must be at least 3 characters")
+            is_valid = False
+        if len(form_data['description']) < 3:
+            flash("Description must be at least 3 characters")
+            is_valid = False
+        if len(form_data['instructions']) < 3:
+            flash("Instructions must be at least 3 characters")
+            is_valid = False
+        return is_valid
 
     @classmethod
     def get_all_recipes(cls):
@@ -37,3 +54,15 @@ class Recipe:
         query = "INSERT INTO recipes (name, description, instructions, date_made, thirty_mins, created_at, updated_at, user_id) VALUES (%(name)s, %(description)s, %(instructions)s, %(date_made)s, %(thirty_mins)s, NOW(), NOW(), %(user_id)s);"
 
         return connectToMySQL('Recipes').query_db( query, data )
+
+    @classmethod
+    def update_recipes(cls, data):
+        query = "UPDATE recipes SET name=%(name)s, description=%(description)s, instructions=%(instructions)s, date_made=%(date_made)s, thirty_mins=%(thirty_mins)s, updated_at=NOW() WHERE recipes.id=%(recipes_id)s;"
+
+        return connectToMySQL('Recipes').query_db(query, data)
+    
+    @classmethod
+    def delete_recipes(cls, data):
+        query = "DELETE FROM recipes WHERE id=%(id)s;"
+
+        return connectToMySQL('Recipes').query_db(query, data)
